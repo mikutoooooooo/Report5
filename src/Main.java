d16fb3f6cf293937a4b932c50de44f855cf34ee9
Mikuto Haga <e215756@ie.u-ryukyu.ac.jp>
2021-12-19 12:49:38 +0900

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
-public class Main {
+class Msg {
+	
+	void Msg(int arg)  {
+		System.out.println(arg);
+	}
+}
+
+class getMessage{
+    void getMessage(int aaa){
+        System.out.println(aaa);
+
+    }
+
+}
+
+class Main {
     public static void main(String[] args)  {
         String str = null;
-        System.out.println(str.length());
+        Msg obj = new Msg();
+        getMessage gM = new getMessage();
+
+        try {
+            obj.Msg(str.length());
+            
+        } catch (NullPointerException e) { 
+            System.out.println("NullPointerExceptionが発生しました。"); 
+            gM.getMessage(345);
+        }
+        
     }
 }
